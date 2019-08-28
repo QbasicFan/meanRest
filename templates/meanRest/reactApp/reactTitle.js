e2515011da1b9c9f18641581ad9b0dccf8f6e405
QbasicFan <philSerme@gmail.com>
2019-08-28 19:36:23 -0400

    Create reactTitle.js

---
+++ templates/meanRest/reactApp/reactTitle.js
@@ -0,0 +1,17 @@
+class Header extends React.Component {
+
+
+  render() {
+    return <div>
+
+                <br/>
+                <br/>
+                <h1 className="heading-secondary">
+                    - Planet Cat fintness -
+                </h1>
+                <br/>
+                <br/>
+            </div>
+  }
+
+}
