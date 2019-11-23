a1a299722bfd67d608846b8aa134ff271f72555a
QbasicFan <philSerme@gmail.com>
2019-11-23 08:37:13 -0500

    Update reactTitle.js

--- templates/meanRest/reactApp/reactTitle.js
+++ templates/meanRest/reactApp/reactTitle.js
@@ -1,14 +1,28 @@
+ {% load staticfiles %}
+
 class Header extends React.Component {
 
 
+
   render() {
+
+
+
+
     return <div>
 
                 <br/>
                 <br/>
-                <h1 className="heading-secondary">
+                <div className="w3-panel w3-blue">
+                <h1 className="w3-xxxlarge w3-text-orange">
                     - Planet Cat fintness -
                 </h1>
+                    <img src= "{% static "img/meanRest/frontmean.jpg" %}"/>
+
+
+
+
+                </div>
                 <br/>
                 <br/>
             </div>
