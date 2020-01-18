74fd612cd4a7c4048e51ff3c8420a0abc59df382
QbasicFan <philSerme@gmail.com>
2020-01-18 12:06:13 -0500

    Update views.py

--- views.py
+++ views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
 # Create your views here.
+
 def index(request):
-   return render(request, 'meanRest/index.html', {} )
+   return render(request, 'food/index.html', {} )
+
